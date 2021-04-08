42cb28b31f3f37d8b08600f2bb308e1d45bba1fb
Faza Thirafi <faza.thirafi@gmail.com>
2021-04-08 15:52:20 +0900

    dummy from server

--- src/trial/ex_summarization.py
+++ src/trial/ex_summarization.py
@@ -3,7 +3,7 @@ import copy
 from ft_encoder import MultiHeadedAttention, PositionalEncoding, PositionwiseFeedForward, EncoderBlock, Encoderi
 
 # Abstractive Summarization with Attention Mechanism
-print("DUMMY")
+print("DUMMY1")
 
 print("DUM2")
 
