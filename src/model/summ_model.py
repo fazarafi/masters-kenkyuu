514e512052ae02dcf19b788af3530e277abd5f7c
Faza Thirafi <faza.thirafi@gmail.com>
2021-06-04 17:07:01 +0900

    Add main RL training class

---
+++ src/model/summ_model.py
@@ -0,0 +1,84 @@
+import pandas
+import numpy as np
+import torch
+
+
+class SummModel(object):
+  """Class of Summarization model. Supports all baseline models"""
+  factcc_rewards = []
+  
+  ac_theta = []
+
+  def calculate_reward(self, reference, summary, measurement):
+    # Calculate reward as for 
+    if 'factcc' in measurement:
+      return self.factcc_cls(reference, summary)
+    if 'feqa' in measurement:
+      return self.feqa_cls(reference, summary)
+    else:
+      return "No evaluation defined"
+
+  def factcc_cls(self, ref, summary):
+    # FactCC Eval Method (Krysynci, 2019)
+    # Feed reference and summary to classification model
+    if (ref==summary):
+      return self.factcc_rewards.append(1)
+    else:
+      return self.factcc_rewards.append(0)
+
+
+  def feqa_cls(self, ref, summary):
+    # FEQA Eval Method ()
+    # Feed reference and summary to classification model
+    if (ref==summary):
+      return 1
+    else:
+      return 0
+
+
+  def decode_model(self, src):
+    return ""
+
+  def loss_function(params):
+    return 0
+    
+  def loss_function_val(params):
+    return 0
+
+  def rl_training(self, summ_data):
+    # Training of reinforcement learining network during using decoding part
+    # Actor-critic algorithm
+    
+    # Parameters initialization
+    k_all = []
+    theta = 0
+    pi = 0
+    a_theta = 0.5
+    a_pi = 0.5
+
+    step = 3
+
+    cnvrgd = False
+    while (not(cnvrgd)):
+      # Pretraining with Critic I
+      
+
+      for i in k_all:
+        # Every k-step of optimization
+        if i % step == 0:
+
+
+        # Critic I optimization
+        theta = theta - a_theta * self.loss_function(theta)
+
+        # Critic II optimization
+        pi = pi - a_pi * self.loss_function_val(summ_data)
+
+        
+
+
+
+
+
+
+
